5a463cf37daac58a55b71dd5a43f3603e135c4f7
Gustavo Vázquez Amado <gustavo@moonsonglabs.com>
2024-05-13 17:37:42 +0200

    Fix Hyperlane Control Group Tests (#19)
    * Fix CCIP control group samples
    
    * Add gas limits to CCIP control group
    
    * fix Hyperlane Samples

--- test/contracts/samples/control/HyperlaneTextSample.sol
+++ test/contracts/samples/control/HyperlaneTextSample.sol
@@ -21,13 +21,13 @@ contract HyperlaneTextSample {
     }
 
     function setRemoteValue(
-        uint16, // destinationChainId,
+        uint16 destinationChainId,
         address destinationAddress,
         bytes calldata payload
     ) external payable {
         // Send message across chains
         MAIL_BOX.dispatch{value: 1}(
-            LOCAL_DOMAIN,
+            destinationChainId,
             bytes32(uint256(uint160(destinationAddress))),
             payload
         );
