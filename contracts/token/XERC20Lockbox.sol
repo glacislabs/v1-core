19dd72f81105360e59833a89580eaedb9cd63686
gusjavaz <gusjavaz@gmail.com>
2024-08-21 17:33:40 +0200

    Fix error verifying some XERC20 contracts

--- contracts/token/XERC20Lockbox.sol
+++ contracts/token/XERC20Lockbox.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4 <0.9.0;
+pragma solidity ^0.8.18;
 
 import {IXERC20} from "../interfaces/IXERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
