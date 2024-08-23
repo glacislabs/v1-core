f69533c15fb20ee32059412c674b9e2af6d17c41
gusjavaz <gusjavaz@gmail.com>
2024-08-23 11:43:24 +0200

    Fix verify/publish problem

--- test/contracts/samples/token/XERC20Sample.sol
+++ test/contracts/samples/token/XERC20Sample.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.18;
 import {XERC20} from "../../../../contracts/token/XERC20.sol";
 
 contract XERC20Sample is XERC20 {
-    constructor(address owner_) XERC20("Sample Token", "SAMPLE", owner_) {
+    constructor(address owner_) XERC20("Glacis xERC20 Sample Token", "GLACIS xERC20 SAMPLE", owner_) {
         _mint(owner_, 10 ** 18);
     }
 }
